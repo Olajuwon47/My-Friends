2c8a64d241c2cfe3e0020c64dd5b004af30d0b4d
Olajuwon47 <lawal.olajuwonwaheed@gmail.com>
2024-07-21 19:07:21 +0100

    hi

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,41 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Tablist from '../components/Tablist.js';
 import SearchBox from '../components/SearchBox.js';
 import Scroll from '../components/Scroll.js';
 import './App.css';
- class App extends Component{
-constructor(){ 
-    super()
-    this.state = {
-    robots: [],
-    searchfield:'',
-}
-}
-componentDidMount(){
+  function App() {
+const [robots, setrobots] =useState([]);
+const [searchfield, setsearchfield] =useState('');
+const [count, setCount] =useState(0);
+useEffect(() => {
     fetch('https://jsonplaceholder.cypress.io/users')
     .then(response=> response.json())
-    .then(users=>this.setState({robots: users }));
+    .then(users=>{setrobots(users) });
+    console.log(count)
+  },[count]);//only run if count changes. 
+const onSearchChange = (event) => {
+    setsearchfield(event.target.value);
 }
-onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value });
-}
-render() {
-     const{robots, searchfield}=this.state;  
     const filteredRobots = robots.filter(robot => {
        return  robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    }) 
+    })
     /*this code is  for multiply users if(robots.length === 0){ */
     return !robots.length?
         <h1>loading</h1>:
     (
         <header className='tc'>
         <h1 className='f1'>wlecome to my robots page</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
+        <button onClick={() => setCount(count + 1)}>
+        Click me!!
+      </button>
+        <SearchBox searchChange={onSearchChange}/>
         <Scroll>
        <Tablist robots = {filteredRobots}/>
        </Scroll>
 </header>
 );      
-}
 }
 export default App;
